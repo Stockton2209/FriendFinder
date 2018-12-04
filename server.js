4a9ed78f4103a83bb4344da63464ca10aa69d720
Alexander <alexstockton09@gmail.com>
2018-12-03 21:35:25 -0600

    created the server and added a listener for connection. Made the homepage route but isn't working so far

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
-
+//Dependencies
+//=============================================
 const express = require("express");
 const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Data
+//=============================================
+
+//Routes
+//=============================================
+//route to the homepage
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+//Listener
+//==============================================
+app.listen(PORT, function(){
+    console.log(`App listening on PORT: ${PORT}`)
+  });
